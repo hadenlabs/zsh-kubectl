1eafce47c5c5bcea9c57a3685a926f342d56a523
Luis Mayta <luis@hadenlabs.com>
2021-12-05 09:55:54 -0500

    chore: implement kubectl functions macro (#7)

--- pkg/base.zsh
+++ pkg/base.zsh
@@ -1,3 +1,14 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+function kubectl::install {
+    kubectl::internal::kubectl::install
+}
+
+function kubectl::plugins::all::install {
+    kubectl::internal::plugins::install
+}
+
+function kubectl::plugin::install {
+    kubectl::internal::plugin::install "${@}"
+}
