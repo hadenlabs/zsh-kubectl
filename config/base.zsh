037dce137265ad4e74a3e297001e8ac84e17b044
Luis Mayta <luis@hadenlabs.com>
2021-12-05 15:53:56 -0500

    chore: implement kube editor vim or nvim (#11)

--- config/base.zsh
+++ config/base.zsh
@@ -5,6 +5,10 @@ export KUBECTL_PACKAGE_NAME=kubectl
 export KUBECTL_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew branch:develop"
 export KUBECTL_MESSAGE_PYENV="Please install brew or use antibody bundle hadenlabs/zsh-pyenv branch:develop"
 export KUBECTL_MESSAGE_NOT_FOUND="this not found installed"
+export KUBE_EDITOR="vim"
+if core::exists nvim; then
+    export KUBE_EDITOR="nvim"
+fi
 export KREW_ROOT="${HOME}/.krew"
 export KREW_ROOT_BIN="${KREW_ROOT}/bin"
 
