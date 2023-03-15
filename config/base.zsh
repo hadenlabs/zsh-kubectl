92ad09569d2548e8fbe52f5aced6b069356a09ee
Luis Mayta <luis@hadenlabs.com>
2023-03-15 15:44:28 -0300

    fix: name functions sync packages (#43)

--- config/base.zsh
+++ config/base.zsh
@@ -3,7 +3,6 @@
 
 export KUBECTL_PACKAGE_NAME=kubectl
 export KUBECTL_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew"
-export KUBECTL_MESSAGE_PYENV="Please install brew or use antibody bundle hadenlabs/zsh-pyenv"
 export KUBECTL_MESSAGE_NOT_FOUND="this not found installed"
 export KUBECTL_MESSAGE_GO_NOT_FOUND="Please install Go or use antibody bundle hadenlabs/zsh-goenv"
 export KUBECTL_LOCAL_PATH_BIN="/usr/local/bin"
